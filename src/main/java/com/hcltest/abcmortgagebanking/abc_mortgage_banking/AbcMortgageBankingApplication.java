6f6327c602e51d83fd4079b7c651c031f1e5a678
jikkskv <jikkskv@gmail.com>
2024-12-07 07:57:26 +0530

    Intial commit with spring boot bare skeleton

---
+++ src/main/java/com/hcltest/abcmortgagebanking/abc_mortgage_banking/AbcMortgageBankingApplication.java
@@ -0,0 +1,13 @@
+package com.hcltest.abcmortgagebanking.abc_mortgage_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcMortgageBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcMortgageBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcltest/abcmortgagebanking/abc_mortgage_banking/AbcMortgageBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcltest.abcmortgagebanking.abc_mortgage_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcMortgageBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
